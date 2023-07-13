223eed2815ac33c0101ac43f50f70a345084c7ee
Zoeyzyy <1162651439@qq.com>
2023-07-14 00:12:01 +0800

    Update: change SelfStartupPlugin::module (delete category)

---
+++ src/window/selfstartupplugin.cpp
@@ -0,0 +1,135 @@
+#include "selfstartupplugin.h"
+
+#include "addbuttonwidget.h"
+#include "defappdetailwidget.h"
+#include "defappmodel.h"
+#include "defappworker.h"
+#include "widgets/widgetmodule.h"
+
+#include <DFloatingButton>
+#include <DStyle>
+
+#include <QApplication>
+#include <QVBoxLayout>
+
+DWIDGET_USE_NAMESPACE
+using namespace DCC_NAMESPACE;
+
+struct DATE
+{
+    QString name;
+    QString displayName;
+    QString icon;
+
+    DATE(const QString &_name,
+         const QString &_displayName,
+         const QString &_icon)
+        : name(_name)
+        , displayName(_displayName)
+        , icon(_icon)
+    {
+    }
+};
+
+QString SelfStartupPlugin::name() const
+{
+    return QStringLiteral("Self Startup");
+}
+
+ModuleObject *SelfStartupPlugin::module()
+{
+    // 一级页面
+    SelfStartupModule *moduleRoot = new SelfStartupModule;
+
+    // 三级页面
+    SelfStartupDetailModule *selfstartupDetail = new SelfStartupDetailModule(moduleRoot->model(), moduleRoot->work());
+    moduleRoot->appendChild(selfstartupDetail);
+
+    // 加号键
+    ModuleObject *addButton =
+            new WidgetModule<AddButtonWidget>("selfstartuplistAddbtn",
+                                              "addSelfStartupApp",
+                                              [moduleRoot](AddButtonWidget *button) {
+                                                  button->setModel(moduleRoot->model());
+                                                  connect(button,
+                                                          &AddButtonWidget::requestCreateFile,
+                                                          moduleRoot->work(),
+                                                          &DefAppWorker::onCreateFile);
+                                              });
+    addButton->setExtra();
+    moduleRoot->appendChild(addButton);
+
+    return moduleRoot;
+}
+
+//|location()|插件位置索引或前一个ModuleObject的name，相同索引则按加载顺序进行排序，先加载的往后顺延，默认追加到最后|
+QString SelfStartupPlugin::location() const
+{
+    return "4";
+}
+
+
+SelfStartupModule::SelfStartupModule(QObject *parent)
+    : VListModule("selfstartup", tr("Self Start-up"), QIcon::fromTheme("dcc_nav_selfstartup"), parent)
+    , m_model(new DefAppModel(this))
+    , m_work(new DefAppWorker(m_model, this))
+    , m_defApps(nullptr)
+{
+}
+
+SelfStartupModule::~SelfStartupModule()
+{
+    m_model->deleteLater();
+    m_work->deleteLater();
+}
+
+void SelfStartupModule::active()
+{
+    m_work->onGetListApps();
+}
+
+DefAppsButtonModule::DefAppsButtonModule(DefAppWorker::DefaultAppsCategory category,
+                                         const QString &name,
+                                         const QString &displayName,
+                                         const QString &icon,
+                                         DefAppModel *model,
+                                         DefAppWorker *work)
+    : PageModule(name, displayName, QVariant::fromValue(icon), nullptr)
+    , m_category(category)
+    , m_model(model)
+    , m_work(work)
+{
+}
+
+DefAppsButtonModule::~DefAppsButtonModule() { }
+
+// QWidget *DefAppsButtonModule::page(){
+//     DefappDetailWidget *defDetail = new DefappDetailWidget(m_category);
+//     defDetail->setModel(m_model);
+
+//    return defDetail;
+//}
+
+// 三级页面
+SelfStartupDetailModule::SelfStartupDetailModule(DefAppWorker::DefaultAppsCategory category,
+                                       DefAppModel *model,
+                                       DefAppWorker *work)
+    : ModuleObject("defappApplistDefapp")
+    , m_category(category)
+    , m_model(model)
+    , m_work(work)
+{
+}
+
+QWidget *SelfStartupDetailModule::page()
+{
+    DefappDetailWidget *defDetail = new DefappDetailWidget(m_category);
+    defDetail->setModel(m_model);
+    // 设置默认程序
+    connect(defDetail,
+            &DefappDetailWidget::requestSetDefaultApp,
+            m_work,
+            &DefAppWorker::onSetDefaultApp);
+    connect(defDetail, &DefappDetailWidget::requestDelUserApp, m_work, &DefAppWorker::onDelUserApp);
+    return defDetail;
+}
--- src/window/selfstartupplugin.h
+++ src/window/selfstartupplugin.h
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2018 - 2023 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
-#ifndef DEFAPPPLUGIN_H
-#define DEFAPPPLUGIN_H
+#ifndef SelfStartupPlugin_H
+#define SelfStartupPlugin_H
 
 #include "defappworker.h"
 #include "interface/pagemodule.h" //to_add!!(below3)
@@ -12,13 +12,13 @@
 class DefAppModel;
 
 // 默认程序插件
-class DefAppPlugin : public DCC_NAMESPACE::PluginInterface
+class SelfStartupPlugin : public DCC_NAMESPACE::PluginInterface
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "com.deepin.dde.ControlCenter.DefApp" FILE "defaultapp.json")
+    Q_PLUGIN_METADATA(IID "com.deepin.dde.ControlCenter.SelfSartup" FILE "selfstartup.json")
     Q_INTERFACES(DCC_NAMESPACE::PluginInterface)
 public:
-    explicit DefAppPlugin() { }
+    explicit SelfStartupPlugin() { }
 
     virtual QString name() const override;
     virtual DCC_NAMESPACE::ModuleObject *module() override;
@@ -26,12 +26,12 @@ public:
 };
 
 // 一级菜单
-class DefAppModule : public DCC_NAMESPACE::VListModule
+class SelfStartupModule : public DCC_NAMESPACE::VListModule
 {
     Q_OBJECT
 public:
-    explicit DefAppModule(QObject *parent = nullptr);
-    ~DefAppModule() override;
+    explicit SelfStartupModule(QObject *parent = nullptr);
+    ~SelfStartupModule() override;
 
     DefAppWorker *work() { return m_work; }
 
@@ -71,11 +71,11 @@ private:
 };
 
 // 三级菜单
-class DefappDetailModule : public DCC_NAMESPACE::ModuleObject
+class SelfStartupDetailModule : public DCC_NAMESPACE::ModuleObject
 {
     Q_OBJECT
 public:
-    DefappDetailModule(DefAppWorker::DefaultAppsCategory category,
+    SelfStartupDetailModule(DefAppWorker::DefaultAppsCategory category,
                        DefAppModel *model,
                        DefAppWorker *work);
     virtual QWidget *page() override;
@@ -86,4 +86,4 @@ private:
     DefAppWorker *m_work;
 };
 
-#endif // DEFAPPPLUGIN_H
+#endif // SelfStartupPlugin_H
