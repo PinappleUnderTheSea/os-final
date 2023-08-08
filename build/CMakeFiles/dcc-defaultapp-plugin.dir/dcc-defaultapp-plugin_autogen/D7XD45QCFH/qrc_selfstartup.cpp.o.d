1becf77a254965ddc66955432f1d4cd232c0e3e4
ZhuJunjie <zhujunjie.4396@bytedance.com>
2023-08-08 16:38:30 +0800

    delete: build

--- build/CMakeFiles/dcc-defaultapp-plugin.dir/dcc-defaultapp-plugin_autogen/D7XD45QCFH/qrc_selfstartup.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/dcc-defaultapp-plugin.dir/dcc-defaultapp-plugin_autogen/D7XD45QCFH/qrc_selfstartup.cpp.o: \
- /home/Pundthsea/Desktop/os-final/build/dcc-defaultapp-plugin_autogen/D7XD45QCFH/qrc_selfstartup.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/dde-control-center.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dde-control-center.
-# This may be replaced when dependencies are built.
