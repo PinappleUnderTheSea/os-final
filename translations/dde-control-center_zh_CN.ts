971c542e4b5838d060ee84e0847382f053e44781
Zoeyzyy <1162651439@qq.com>
2023-08-09 21:28:58 +0800

    Fix: debug description CN

--- translations/dde-control-center_zh_CN.ts
+++ translations/dde-control-center_zh_CN.ts
@@ -3897,4 +3897,11 @@ UnionTech Software is committed to research and improve the security, accuracy a
         <translation>自启动程序</translation>
     </message>
 </context>
+<context>
+    <name>SelfStartupPlugin</name>
+    <message>
+        <source>Self Start-up</source>
+        <translation>自启动程序</translation>
+    </message>
+</context>
 </TS>
