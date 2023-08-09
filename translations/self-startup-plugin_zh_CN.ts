3cf77755e9f91ee526c9286cf9d31f5e640b6ae3
Zoeyzyy <1162651439@qq.com>
2023-08-09 21:48:37 +0800

    Fix: cmakelist debug

--- translations/self-startup-plugin_zh_CN.ts
+++ translations/self-startup-plugin_zh_CN.ts
@@ -6,4 +6,11 @@
         <translation>自启动程序</translation>
     </message>
 </context>
+<context>
+    <name>SelfStartupPlugin</name>
+    <message>
+        <source>Self Start-up</source>
+        <translation>自启动程序</translation>
+    </message>
+</context>
 </TS>
